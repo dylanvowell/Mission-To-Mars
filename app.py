0b31790db9ea9a2217cd9c3a478249a239a0b96c
Dylan Vowell <vowell.dylan@gmail.com>
2022-03-01 09:51:12 -0700

    added challenge things

--- app.py
+++ app.py
@@ -3,18 +3,20 @@ from flask_pymongo import PyMongo
 from bs4 import BeautifulSoup as soup
 import scraping
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder="templates")
 
 # Use flask_pymongo to set up mongo connection
 app.config["MONGO_URI"] = "mongodb://localhost:27017/mars_app"
 mongo = PyMongo(app)
 
 @app.route("/")
+
 def index():
    mars = mongo.db.mars.find_one()
    return render_template("index.html", mars=mars)
 
 @app.route("/scrape")
+
 def scrape():
    mars = mongo.db.mars
    mars_data = scraping.scrape_all()
@@ -22,4 +24,4 @@ def scrape():
    return redirect('/', code=302)
 
 if __name__ == "__main__":
-   app.run()
+   app.run(debug=True)
--- scraping.py
+++ scraping.py
@@ -19,7 +19,8 @@ def scrape_all():
         "news_paragraph": news_paragraph,
         "featured_image": featured_image(browser),
         "facts": mars_facts(),
-        "last_modified": dt.datetime.now()
+        "last_modified": dt.datetime.now(),
+        "mars_hemispheres": hemisphere_image_urls(browser)
     }
 
     # Stop webdriver and return data
@@ -96,6 +97,29 @@ def mars_facts():
 
     # Convert dataframe into HTML format, add bootstrap
     return df.to_html(classes="table table-striped")
+
+
+def hemisphere_image_urls(browser):
+    # 1. Use browser to visit the URL 
+    url = 'https://marshemispheres.com/'
+    browser.visit(url)
+
+    # 2. Create a list to hold the images and titles.
+    hemisphere_image_urls = []
+
+    for info in range(4):
+        hemispheres={}
+
+        get_images=browser.find_by_tag('h3')[info]
+        get_images.click()
+        html = browser.html
+        img_soup = soup(html, 'html.parser')
+        hemispheres["URL"] =url+img_soup.find('img', class_='wide-image').get('src')
+        hemispheres["TITLE"] = img_soup.find('h2', class_='title').text
+        hemisphere_image_urls.append(hemispheres)
+        browser.back()
+
+    return hemisphere_image_urls
 
 if __name__ == "__main__":
 
